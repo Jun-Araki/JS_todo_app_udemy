4ee09ae977110171cb88a1832ca384692c58a729
Jun Araki <105146615+Jun-Araki@users.noreply.github.com>
2025-01-26 04:20:34 +0000

    first

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,28 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+const onClickAdd = () => {
+  // テキストボックスの内容を取得し、初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を追加
+  div.appendChild(p);
+  li.appendChild(div);
+
+  //  未完了リストへの追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
